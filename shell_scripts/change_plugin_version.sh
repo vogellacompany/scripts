f0570b5e66c87a81e5973af24db88810d74fb426
Fabian Pfaff <fabian.pfaff@vogella.com>
2017-05-09 12:46:47 +0200

    Adds shell script to change asciidoctor gradle plugin version

---
+++ shell_scripts/change_plugin_version.sh
@@ -0,0 +1,18 @@
+# changes the version of the vogella asciidoctor gradle plugin to the specified version
+# https://github.com/vogellacompany/com.vogella.gradle.plugins
+for dir in ./*/
+do
+    if [ "$dir" == './com.vogella.tutorials.scripts/' ]
+    then
+        continue
+    fi
+    dir=${dir%*/}
+    cd ${dir}
+    if [ -f 'build.gradle' ]
+    then
+        echo "build.gradle found in ${dir}"
+        sed -i "s/'com.vogella.gradle:com.vogella.gradle.plugin.asciidoc:.*\..*\..*'/'com.vogella.gradle:com.vogella.gradle.plugin.asciidoc:$1'/" build.gradle
+    fi
+    cd ..
+done
+
