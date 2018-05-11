6fd484e0f80e3cd5d99ceda1f9c5163c5c2fd1ff
Fabian Pfaff <459631+fap-@users.noreply.github.com>
2018-05-11 21:44:47 +0200

    Monitoring.sh: extract receiver list to variable

--- shell_scripts/monitoring.sh
+++ shell_scripts/monitoring.sh
@@ -2,7 +2,8 @@
 
 URL=https://learn.vogella.com/login
 SEARCH_STRING="Learning Portal"
+RECEIVER=lars.vogel@vogella.com,jonas.hungershausen@vogella.com,fabian.pfaff@vogella.com
 
 if [ "$(curl -s $URL | grep "$SEARCH_STRING")" == "" ] ; then
-    echo "$(date) ALARM: Learning Portal looks down - string \"$SEARCH_STRING\" couldn't be found in server answer." | mail -s '[learn.vogella.com] ALARM: Learning Portal looks down' lars.vogel@vogella.com,jonas.hungershausen@vogella.com,fabian.pfaff@vogella.com
+    echo "$(date) ALARM: Learning Portal looks down - string \"$SEARCH_STRING\" couldn't be found in server answer." | mail -s '[learn.vogella.com] ALARM: Learning Portal looks down' "$RECEIVER"
 fi
